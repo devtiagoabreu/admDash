2b5fabe88b8120e9ef2da2b3f2687957895b5735
Tiago de Abreu <devtiagoabreu@gmail.com>
2023-01-27 14:49:56 -0300

    teste:  consumindo uma api de posts aberta

--- react/axios/src/routes/Home.jsx
+++ react/axios/src/routes/Home.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import "./Home.css";
+
+
 
 const Home = () => {
+  
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async() => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+        ); 
+
+        const data = response.data;
+
+        setPosts(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=> {
+    getPosts()
+  }, [])
+  
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      
+      {/* exemplo em uma tabela
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+        <table className="table">
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Post</th>
+          </tr>
+          <tr>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>{post.body}</td>
+          </tr>
+        </table>
+      ))
+      )}
+        */}
+
+
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
 
 export default Home
